5c6a1f6a32a10193a5db93500173e6652615a7c0
EmersonCDias <emerson_cdias@hotmail.com>
2021-01-23 13:13:27 -0300

    Creating header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const DashBoard: React.FC = () => <h1>Oi</h1>;
+import { useAuth } from '../../hooks/AuthHook';
+import { Containter, Header, HeaderContent, Profile } from './styles';
+import logo from '../../assets/logo.svg';
+
+const DashBoard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Containter>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Containter>
+  );
+};
 
 export default DashBoard;
